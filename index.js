a65d5cd05cc832ab24914faebe81f9c12b33f568
Prabeshika <prabeshika14@gmail.com>
2023-11-22 14:44:18 +0100

    Third Iteration : loops

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Iteration 1: Names and Input
-let hacker1 = "XXXX";
+let hacker1 = "Oliver";
 console.log(`The driver's name is ${hacker1}`);
 
-let hacker2 = "YYYY";
+let hacker2 = "Daniel";
 console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
@@ -21,3 +21,34 @@ if (hacker1.length > hacker2.length) {
 }
 
 // Iteration 3: Loops
+//Print the characters of the driver's name, separated by space, and in capital letters, i.e., "J O H N"
+let formattedName = " ";
+
+for (let i = 0; i < hacker1.length; i++) {
+  formattedName += hacker1[i].toUpperCase() + " ";
+}
+console.log(formattedName);
+
+//Print all the characters of the navigator's name in reverse order, i.e., "nhoJ".
+const str = "john";
+let reversedStr = "";
+for (let i = str.length - 1; i >= 0; i--) {
+  reversedStr += str[i];
+}
+console.log(reversedStr);
+
+/*Depending on the lexicographic order of the strings, print:
+
+The driver's name goes first.
+Yo, the navigator goes first, definitely.
+What?! You both have the same name? */
+
+const result = hacker1.localeCompare(hacker2);
+
+if (result < 0) {
+  console.log(`The driver's name goes first`);
+} else if (result > 0) {
+  console.log(`Yo, the navigator goes first, definately`);
+} else {
+  console.log(`what?! You both have same name?`);
+}
